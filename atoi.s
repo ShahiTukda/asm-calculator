a0a2ebc5cab6bb030a9d18669861cd9535b1b983
Arjun <muhehehaha8@gmail.com>
2026-08-16 20:50:24 +0530

    Create atoi.s
    Converts ASCII strings to Integers.

---
+++ atoi.s
@@ -0,0 +1,47 @@
+mov rcx, 0
+mov rax, 0
+push rbx
+mov rbx, 0
+cmp byte ptr [rdi], 0x2d
+je rcxinc1
+jmp loop
+loop:
+imul rax, 10
+movzx rbx, byte ptr [rdi+rcx]
+add rax, rbx
+cmp byte ptr [rdi+rcx], 0x39
+ja done
+cmp byte ptr [rdi+rcx], 0x30
+jb done
+sub rax, 0x30
+inc rcx
+cmp byte ptr [rdi+rcx], 0x39
+ja done
+cmp byte ptr [rdi+rcx], 0x30
+jb done
+jmp loop
+done:
+pop rbx
+ret
+rcxinc1:
+inc rcx
+jmp negloop
+negloop:
+imul rax, 10
+movzx rbx, byte ptr [rdi+rcx]
+add rax, rbx
+cmp byte ptr [rdi+rcx], 0x39
+ja negdone
+cmp byte ptr [rdi+rcx], 0x30
+jb negdone
+sub rax, 0x30
+inc rcx
+cmp byte ptr [rdi+rcx], 0x39
+ja negdone
+cmp byte ptr [rdi+rcx], 0x30
+jb negdone
+jmp negloop
+negdone:
+neg rax
+pop rbx
+ret
